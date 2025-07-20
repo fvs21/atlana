f6ba65e02516c510a7b488f2db732d66d22fa2ae
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-07-20 15:53:41 -0600

    Fixed requirements

--- frontend/app/features/professor-rating/components/ProfessorsList/index.tsx
+++ frontend/app/features/professor-rating/components/ProfessorsList/index.tsx
@@ -3,8 +3,7 @@ import styles from "./styles.module.scss";
 export default function ProfessorsList() {
     return (
         <div className={styles.professorsListContainer}>
-            
+            s
         </div>
     )
 }
-
